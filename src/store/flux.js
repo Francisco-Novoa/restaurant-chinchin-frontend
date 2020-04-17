a496aa26668084e3352050854c2011ca805e14bd
Francisco Novoa Parada <francisco.novoa.14@sansano.usm.cl>
2020-04-17 09:31:16 -0400

    first commit

---
+++ src/store/flux.js
@@ -0,0 +1,13 @@
+
+export default function getState({ getStore, getActions, setStore }) {
+    return {
+        store: {
+           //store variables goes here
+        },
+        actions: {
+           //actions go here.
+            },
+
+        }
+    }
+}
