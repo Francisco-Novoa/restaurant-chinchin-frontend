e48cb336e40d21eaf97c514ce23882cd120492ff
Harinson Arellan Itanare <harrinson.arellan@gmail.com>
2020-04-29 09:33:50 -0400

    cambios nuevos

--- src/components/Restaurant/restaurant_card.js
+++ src/components/Restaurant/restaurant_card.js
@@ -9,11 +9,7 @@ export default function RestaurantCard(props) {
                         <img src="https://picsum.photos/200/200?random=1"/>
                         <div className="card-body">
                             <h5 className="card-title">{props.restaurant.name}</h5>
-<<<<<<< HEAD
                             <a className="btn btn-primary text-white"><i className="fas fa-shopping-cart"></i></a>
-=======
-                            <botton className="btn btn-primary text-white">saber mas </botton>
->>>>>>> 6a8dab2a4b887033f05158f1985573631c0d4a75
                         </div>
                     </div>
             </Link>
